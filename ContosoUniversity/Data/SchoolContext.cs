e9e7e3527e74a2f601b62eded2949433a5da5037
hungnh <hungnh@huongvietgroup.com>
2018-11-03 11:43:13 +0700

    up

---
+++ ContosoUniversity/Data/SchoolContext.cs
@@ -0,0 +1,24 @@
+﻿using ContosoUniversity.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Data
+{
+    public class SchoolContext : DbContext
+    {
+
+        public SchoolContext(DbContextOptions<SchoolContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+
+
+    }
+}
